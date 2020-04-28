28d85ce3c2dba3017a9f81056a915636321d0f5b
Kauê Murakami <kauetmurakami@gmail.com>
2020-04-28 19:44:43 -0300

    chaveando entre sistemas operacionais

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:imcbloc/ui/android/material-app.dart';
 import 'package:imcbloc/ui/ios/cupertino-app.dart';
+import 'dart:io' show Platform;
 
-void main() => runApp(MyMaterialApp());
-//void main() => runApp(MyCupertinoApp());
+void main() =>
+Platform.isAndroid ?  runApp(MyMaterialApp()) : runApp(MyCupertinoApp());
+
+
