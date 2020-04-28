2cde83857547da0e0f115c043a7f9acd4bdbec8e
Kauê Murakami <kauetmurakami@gmail.com>
2020-04-28 19:16:06 -0300

    criando estrutura e páginas para diferentes os

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:imcbloc/ui/android/material-app.dart';
+import 'package:imcbloc/ui/ios/cupertino-app.dart';
 
-void main() => runApp(MyApp());
-
+void main() => runApp(MyMaterialApp());
+//void main() => runApp(MyCupertinoApp());
